22c401573c198e120855bed6429cc23a356e543b
Joel Eriksson <joel.eriksson3@itggot.se>
2016-12-05 17:43:44 +0100

    Added firebase

---
+++ site/js/main.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var firebaseConfig = {
+	apiKey: "AIzaSyCxKfqq6nSq81SaKB6kKPPX-esINfmMqsA",
+	authDomain: "cardgame-webweu02.firebaseapp.com",
+	databaseURL: "https://cardgame-webweu02.firebaseio.com",
+	storageBucket: "cardgame-webweu02.appspot.com",
+	messagingSenderId: "1011486029280"
+};
+firebase.initializeApp(config);
